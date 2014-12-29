63e91e49e3ada3a6ce73735ff6a1c02d12852491
aweika <kller@foxmail.com>
2014-12-29 23:53:27 +0800

    更改插件基本信息

--- kl_data_call.php
+++ kl_data_call.php
@@ -2,12 +2,12 @@
 /*
 Plugin Name: 数据调用
 Version: 3.0
-Plugin URL: http://kller.cn/?post=70
+Plugin URL: https://github.com/aweika/kl_data_call
 Description: 实现emlog相关的数据调用功能。
 ForEmlog: 5.x版本
-Author: KLLER
+Author: 阿维卡
 Author Email: kller@foxmail.com
-Author URL: http://kller.cn
+Author URL: http://www.aweika.com
 */
 !defined('EMLOG_ROOT') && exit('access deined!');
 
